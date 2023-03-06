7adcaa479c7e008a672b9d5ef9d909f4ad625d8f
George Jempty <consult@dexygen.com>
2023-03-06 13:25:50 +0000

    Ensure property images are centered in modal, Phoenix 7 was askew, also deleted a1bc 'staging' directory

--- abc/index.php
+++ abc/index.php
@@ -232,10 +232,11 @@
                   <section class="modal-card-body columns">
                     <p class="column">
                     <!-- the image's src gets set on the mouseover event -->
-                    <img class="image abc-property-img" src="">
+                    <img class="image abc-property-img is-inline-block" src="">
                     </p>
                   </section>
                   <footer class="modal-card-foot abc-property-img-modal-footer columns is-flex-mobile">
+                    <!-- pb-6 class below doesn't seem correct but "works" -->
                     <button class="button abc-property-img-modal-close pb-6 column is-narrow-mobile is-2 is-offset-5 is-link is-large">
                       CLOSE
                     </button>
