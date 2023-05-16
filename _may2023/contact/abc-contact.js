0f772fc9d8b825130315430d454d1f1a14477895
George Jempty <consult@dexygen.com>
2023-05-16 10:35:56 +0100

    Create new work in progress site

---
+++ _may2023/contact/abc-contact.js
@@ -0,0 +1,220 @@
+{
+    const contactModal = document.querySelector(".abc-contact-modal");
+    const contactButtons = document.querySelectorAll(".abc-contact-button");
+
+    // BEGIN: declarations outside of contactButton/addEventListener/setTimeout
+    let contactForm;
+    let formMessagesCloseButton;
+    let formSubmitButton;
+    let formResetButton;
+    let formSubmitSuccessMsg;
+    let formSubmitSuccessCloseButton;
+    let formPostResponseErrorMsg;
+    let formPostResponseErrorCloseButton;
+    
+    let formErrorMsgContainer;
+    let formErrorMsgOnName;
+    let formErrorMsgOnEmail;
+    let formErrorMsgOnStartDate;
+    let formErrorMsgOnDuration;
+
+    let formFieldDelivery;
+    let formFieldProperty;
+    let formFieldStartDate;
+    let formFieldDuration;
+    // END    
+
+    let eventListenersInitialized;
+    const INSUFFICIENT_INFO = "insufficientInfo";
+
+    [...contactButtons].forEach(closeButton => {  
+        closeButton.addEventListener("click", () => {
+            setTimeout(() => { // waits for modal and its contents to render
+                contactModal.classList.add("is-active");
+                
+                // only add event listeners once
+                if (!eventListenersInitialized) {
+                    eventListenersInitialized = true;
+                    
+                    const modalCloseButtons = document.querySelectorAll(".abc-modal-close");
+                    [...modalCloseButtons].forEach(closeButton => {  
+                        closeButton.addEventListener("click", () => {
+                            contactModal.classList.remove("is-active");
+                            contactForm.reset();
+                            hideNotifications();
+                        });
+                    });
+    
+                    contactForm = document.querySelector(".abc-contact-form");
+                    formSubmitButton = document.querySelector(".abc-contact-form-submit");
+                    formResetButton = document.querySelector(".abc-contact-form-reset");
+
+                    formErrorMsgContainer = contactModal.querySelector(".abc-contact-form-error-msg-container");
+                    formMessagesCloseButton = contactModal.querySelector(".abc-contact-form-messages-close-button");
+                    formSubmitSuccessMsg = contactModal.querySelector(".abc-contact-form-success-msg");
+                    formSubmitSuccessCloseButton = contactModal.querySelector(".abc-contact-form-success-msg-close-button");
+                    formPostResponseErrorMsg = contactModal.querySelector(".abc-contact-form-post-response-msg");
+                    formPostResponseErrorCloseButton = contactModal.querySelector(".abc-contact-form-post-response-close-button");
+
+                    /*
+                      because of the naming convention, the following can eventually 
+                      be selected with querySelectorAll and the results put into an object
+                    */
+                    formErrorMsgOnName = contactModal.querySelector(".abc-contact-form-error-name");
+                    formErrorMsgOnEmail = contactModal.querySelector(".abc-contact-form-error-email");
+                    formErrorMsgOnStartDate = contactModal.querySelector(".abc-contact-form-error-start-date");
+                    formErrorMsgOnDuration = contactModal.querySelector(".abc-contact-form-error-duration");
+                    formErrorMsgOnInsufficientInfo = contactModal.querySelector(".abc-contact-form-error-insufficient-info");
+    
+                    // same comment from above applies
+                    formFieldDelivery = contactForm.querySelector(".abc-contact-form-field-delivery");
+                    formFieldProperty = contactForm.querySelector(".abc-contact-form-field-property");
+                    formFieldStartDate = contactForm.querySelector(".abc-contact-form-field-start-date");
+                    formFieldDuration = contactForm.querySelector(".abc-contact-form-field-duration");
+                    
+                    // BEGIN: Only allow Delivery checkbox OR property selection menu, to have a value
+                    formFieldDelivery.addEventListener("click", () => {
+                        if (formFieldDelivery.checked) {
+                            formFieldProperty.value = "";
+                        }
+                    });
+                    formFieldProperty.addEventListener("change", () => {
+                        if (formFieldProperty.value) {
+                            formFieldDelivery.checked = false;
+                        }
+                    })
+                    // END
+    
+                    formMessagesCloseButton.addEventListener("click", () => {
+                        resetFormErrorMessages();
+                    });
+    
+                    formSubmitSuccessCloseButton.addEventListener("click", () => {
+                        formSubmitSuccessMsg.classList.add("is-hidden");
+                        contactModal.classList.remove("is-active");
+                        contactForm.reset();
+                    });
+
+                    formPostResponseErrorCloseButton.addEventListener("click", () => {
+                        formPostResponseErrorMsg.classList.add("is-hidden");
+                    });
+    
+                    formResetButton.addEventListener("click", () => contactForm.reset());
+    
+                    formSubmitButton.addEventListener("click", () => {
+                        hideNotifications();
+                        resetFormErrorMessages();
+                        let errorMessages = contactFormErrorMessages(contactForm);
+    
+                        if (errorMessages) {
+                            formErrorMsgContainer.classList.remove("is-hidden");
+                            formErrorMsgContainer.scrollIntoView();
+                            
+                            errorMessages.forEach((msg) => {
+                                if (msg === "name") {
+                                    formErrorMsgOnName.classList.remove("is-hidden");
+                                }
+                                if (msg === "email") {
+                                    formErrorMsgOnEmail.classList.remove("is-hidden");
+                                }
+                                if (msg === "startDate") {
+                                    formErrorMsgOnStartDate.classList.remove("is-hidden");
+                                }
+                                if (msg === "duration") {
+                                    formErrorMsgOnDuration.classList.remove("is-hidden");
+                                }
+                                if (msg === INSUFFICIENT_INFO) {
+                                    formErrorMsgOnInsufficientInfo.classList.remove("is-hidden");
+                                }
+                            });
+                        }
+                        else {
+                            let payload = JSON.stringify(Object.fromEntries(new FormData(contactForm)));
+                            // console.log(payload); return;
+    
+                            axios.post("./contact/send_email.php", payload)
+                                .then(response => {
+                                    if (response.status === 200 && response.data.mail_success) {
+                                        formSubmitSuccessMsg.classList.remove("is-hidden");
+                                        formSubmitSuccessMsg.scrollIntoView();
+    
+                                        setTimeout(() => {
+                                            formSubmitSuccessMsg.classList.add("is-hidden");
+                                            contactModal.classList.remove("is-active");   
+                                            contactForm.reset();                                     
+                                        }, 6000);
+                                    }
+                                    else {
+                                        handlePostResponseErrors(response.status);
+                                    }
+                                })
+                                .catch(err => { 
+                                    handlePostResponseErrors(err);
+                                });              
+                        }
+    
+                        function handlePostResponseErrors() {
+                            formPostResponseErrorMsg.classList.remove("is-hidden");
+                            formPostResponseErrorMsg.scrollIntoView();
+                        }
+                    });
+                }
+            });
+    
+            function contactFormErrorMessages(contactForm) {
+                let errorMessages;
+                
+                const formData = new FormData(contactForm);
+                const emailRegex = /([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])/;
+                // as to the above, standards-based, regex, see: https://stackoverflow.com/a/26989421/34806
+    
+                for (let [fieldName, val] of formData.entries()) {
+                  if (fieldName === "name" && val.trim().length < 2) {
+                    errorMessages = errorMessages || [];
+                    errorMessages.push(fieldName);
+                  }
+                  if (fieldName === "email" && !emailRegex.test(val.trim())) {
+                    errorMessages = errorMessages || [];
+                    errorMessages.push(fieldName);
+                  }
+                  if (fieldName === "startDate" && val !== "") {
+                    const today = new Date();
+                    today.setHours(0,0,0,0); // sets to first millisecond of today's date
+                    if (new Date(val) < today) {
+                      errorMessages = errorMessages || [];
+                      errorMessages.push(fieldName);          
+                    }
+                  }
+                  if (fieldName === "duration" && val <= -1) {
+                    errorMessages = errorMessages || [];
+                    errorMessages.push(fieldName);
+                  }
+                }
+    
+                if (!errorMessages) { // check for any information besides just name and email     
+                    const formEntriesArray = Array.from(formData.entries());
+                    const hasSomeAdditionalInfo = formEntriesArray.filter(keyValPair => 
+                      keyValPair[0] !== "name" && keyValPair[0] !== "email"    
+                    ).some(keyValPair => keyValPair[1].trim());
+                    if (!hasSomeAdditionalInfo) {
+                        errorMessages = errorMessages || [];
+                        errorMessages.push(INSUFFICIENT_INFO);
+                    }
+                }
+    
+                return errorMessages;
+            }
+    
+            function resetFormErrorMessages() {
+                formErrorMsgContainer.classList.add("is-hidden");
+                const errorMsgs = formErrorMsgContainer.querySelectorAll("[class^='abc-contact-form-error-']");
+                errorMsgs.forEach((errorMsgEl) => { errorMsgEl.classList.add("is-hidden") });
+            }
+
+            function hideNotifications() {
+                formSubmitSuccessMsg.classList.add("is-hidden");
+                formPostResponseErrorMsg.classList.add("is-hidden");
+            }
+        });        
+    });
+}
---
+++ _may2023/contact/send_email.php
@@ -0,0 +1,54 @@
+<?php
+require_once('../jrmvc.lib.php');
+
+class ContactEmailController extends AbstractJrMvcController {  
+    function applyInputToModel() {
+        require_once('../_inc_property_details.php');
+        $payload = json_decode(file_get_contents('php://input'), true);
+        
+        $name = $payload["name"];
+        $email = $payload["email"];
+        $property = $payload["property"];
+        $startDate = $payload["startDate"];
+        $duration = $payload["duration"];
+        $other = $payload["other"];
+
+        if (array_key_exists("delivery", $payload)) {
+            $serviceType = "DELIVERY";
+        }
+        elseif ($property) {
+            # only reach this if $property is not zero, the "placeholder"
+            # $abcPropertyNamesAll had value unshifted onto front 
+            # for properties select menu, so subtract 1 for correct index
+            $serviceType = $abcPropertyNamesAll[$property - 1];
+        }
+        else {
+            $serviceType = "Neither indicated";
+        }
+
+        $to = "albeachchairs@gmail.com, jemptymethod@gmail.com";
+        $subject = "Inquiry from albeachchairs.com from: $name";
+        
+        $messageLines = Array(
+            "Email: $email",
+            "Service: $serviceType",
+            "Start Date: $startDate",
+            "Duration: $duration",
+            "Other: $other"
+        );
+
+        $message = join("\n", $messageLines);
+        $mail_success = mail($to, $subject, $message);
+
+        $mto = new class(JrMvcMTO::NULL_TPL) extends JrMvcMTO {
+            function onNullTemplate() {
+                echo json_encode($this->model);
+            }
+        };        
+        $mto->setModelValue("mail_success", $mail_success ? 1 : 0);
+        return $mto;
+    }
+}
+
+ContactEmailController::sendResponse(new ContactEmailController());
+?>
