6b0de5d22b3d588923440587d8d0a15ad63fd4e3
George Jempty <consult@dexygen.com>
2023-05-17 09:40:22 +0100

    Created classes for new palette and used palette for contact methods

--- _may2023/contact/_inc_contact_component.php
+++ _may2023/contact/_inc_contact_component.php
@@ -5,17 +5,30 @@ $isPhone = stripos($ua, 'Android') || stripos($ua, 'iPhone');
 if ($isPhone) :?>
   <div class="is-size-5 font-pangolin pt-1 mb-1">Tap a contact method below</div>
   <div>
-    <a href="tel:2512334000" class="button is-rounded is-link is-size-3 is-underlined mb-2">
+    <a href="tel:2512334000" class="button 
+                                    abc-contact-button 
+                                    is-rounded 
+                                    abc-background-seafoam 
+                                    abc-text-orange 
+                                    is-size-3 
+                                    is-underlined
+                                    mb-2">
       251-233-4000
     </a>
   </div>
 <?php else :?>
   <div class="is-size-5 font-pangolin pt-1">Call or click to fill out form</div>
-  <div class="is-size-2 has-text-link">251-233-4000</div>  
+  <div class="is-size-2 has-text-primary-dark has-text-weight-semibold">251-233-4000</div>  
 <?php endif; ?>
 
 <div>
-  <a class="button abc-contact-button is-rounded is-link is-size-3 is-underlined">
+  <a class="button 
+            abc-contact-button 
+            is-rounded 
+            abc-background-seafoam 
+            abc-text-orange 
+            is-size-3 
+            is-underlined">
     <div class="mx-2">Contact Form</div>
   </a>
 </div>
