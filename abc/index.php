bb5ac79173df789d8612081be5fd7965da8584a6
George Jempty <consult@dexygen.com>
2023-05-24 17:12:15 +0100

    Move everything to main /abc/ directory and delete working directories

--- abc/index.php
+++ abc/index.php
@@ -2,7 +2,6 @@
 <html lang="en">
   <head>
     <!-- Google Tag Manager -->
-	
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -18,6 +17,9 @@
     </style>
     <link rel="stylesheet" href="./assets/css/bulma.min.css" type="text/css">
     <link rel="stylesheet" href="./assets/css/abc.css" type="text/css">
+    </script>
+    <script src="https://kit.fontawesome.com/936996b1d1.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.4/axios.min.js" integrity="sha512-LUKzDoJKOLqnxGWWIBM4lzRBlxcva2ZTztO8bTcWPmDSpkErWx0bSP4pdsjNH8kiHAUPaT06UXcb+vOEZH+HpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   </head>
   <body id="abc-viewport"><html>
     <!-- Google Tag Manager (noscript) -->
@@ -38,26 +40,35 @@
             </div>
           </div>
           <div class="mb-3 font-pangolin abc-header-links">
-            <a class="is-size-2 is-underlined" href="tel:2512334000">251-233-4000</a>
-            <div class="has-text-danger-dark is-block is-size-3">
-              <a class="has-text-danger-dark is-underlined" href="#abc-delivery">
+            <?php
+              $contact_modal_created = false; 
+              require("./contact/_inc_contact_component.php"); 
+            ?>
+            <div class="is-block is-size-3 mt-3">
+              <a href="#abc-delivery"  class="has-text-danger-dark 
+                                              has-text-weight-semibold 
+                                              font-kalam
+                                              is-underlined">
                 We deliver to YOUR location
               </a>
             </div>
-            <div class="is-size-4 has-text-primary-dark ">
-              OR reserve at <a class="is-underlined" href="#abc-properties">properties we serve</a> 
-              <div class="is-italic">
-                (NO delivery fees apply at <a class="is-underlined" href="#abc-properties">THESE condos)</a>
-              </span>
+            <div class="is-size-4 has-text-primary-dark">
+              OR if you're staying at
+              <div><a class="is-underlined has-text-primary-dark" href="#abc-properties">Properties we serve</a></div>
+              <div>
+                (No delivery fees apply) 
+              </div>
             </div>
           </div>
           <div id="abc-carousel" class="mt-2 mb-5">
             <!-- 
               Appending small query string to each image url to force refresh 
               especially after uploading and on mobile, if these images are altered,
-              e.g. resized, the query string "h=180" should be changed
+              e.g. resized, the query string "h=180" should be changed, e.g. perhaps
+              appending the date: "h=180-apr01-2023", or some such, or do programmatically
             -->
-            <div class="is-block"><img src="./assets/images/slideshow/delivery-kit.jpg?h=180"></img></div>
+            <div class="is-block"><img src="./assets/images/slideshow/flag-at-beach.jpg?h=180"></img></div>
+            <div class="is-hidden"><img src="./assets/images/slideshow/delivery-kit.jpg?h=180"></img></div>
             <div class="is-hidden"><img src="./assets/images/slideshow/gull-over-chairs.jpg?h=180"></img></div>
             <div class="is-hidden"><img src="./assets/images/slideshow/sunrise.jpg?h=180"></img></div>
             <div class="is-hidden"><img src="./assets/images/slideshow/beachside.jpg?h=180"></img></div>
@@ -67,51 +78,69 @@
           <div class="pt-2 has-text-danger-dark is-size-4 has-text-weight-semibold font-kalam">
             Alabama Beach Chairs
           </div>
-          <div class="mb-1 pb-1 is-size-5 is-hidden-tablet is-underlined has-text-primary-dark has-text-weight-bold abc-toggle-menu-mobile">
+          <div class="mb-1 
+                      pb-1 
+                      is-size-5 
+                      is-hidden-tablet 
+                      is-underlined 
+                      has-text-danger-dark
+                      has-text-weight-bold 
+                      abc-toggle-menu-mobile">
             <div>Show Menu</div>
           </div>
-          <div class="columns abc-menu-links py-2 is-size-4-mobile is-hidden-mobile is-size-4-desktop has-text-weight-semibold">
-            <a class="column is-one-fifth py-1" href="#abc-delivery">Delivery Service</a>
-            <a class="column is-one-fifth py-1" href="#abc-condo-service">Condo Service</a>
-            <a class="column is-one-fifth py-1" href="#abc-featured-property">Featured Property</a>
-            <a class="column is-one-fifth py-1" href="#abc-properties">Properties</a>
-            <a class="column is-one-fifth py-1" href="#abc-about-contact">About/Contact</a>
+          <div class="columns 
+                      abc-menu-links 
+                      py-2 
+                      is-size-4-mobile 
+                      is-hidden-mobile 
+                      is-size-4-desktop 
+                      has-text-weight-semibold">
+            <a class="column is-one-fifth py-1 has-text-primary-dark" href="#abc-delivery">Delivery Service</a>
+            <a class="column is-one-fifth py-1 has-text-primary-dark" href="#abc-condo-service">Condo Service</a>
+            <a class="column is-one-fifth py-1 has-text-primary-dark" href="#abc-featured-property">Featured Property</a>
+            <a class="column is-one-fifth py-1 has-text-primary-dark" href="#abc-properties">Properties</a>
+            <a class="column is-one-fifth py-1 has-text-primary-dark" href="#abc-about-contact">About/Contact</a>
           </div>
         </nav>
         <!-- DELIVERY -->
         <section class="pt-2 mb-6 mt-5 abc-section" id="abc-delivery">
           <div class="pt-3">
             <header class="columns is-mobile is-centered mb-2">
-              <div class="column is-narrow is-size-4 has-background-danger-dark has-text-danger-light font-kalam">
+              <div class="column is-narrow is-size-3 has-background-danger-dark has-text-danger-light font-kalam">
               DELIVERY
               </div>
             </header>
             <article class="columns is-mobile is-centered font-pangolin">
               <div class="column is-10 has-text-centered">
-                <div class="is-flex is-justify-content-center mb-2">
-                  <div class="has-background-link has-text-warning is-size-3 font-pangolin px-2">
+                <div class="is-flex is-justify-content-center my-3">
+                  <div class="has-background-primary-dark has-text-warning is-size-3 font-pangolin px-2">
                     CONTACT US FOR DELIVERY
                   </div>
                 </div>
-                <div class="is-size-2 font-pangolin abc-phone">
-                  <a class="is-size-2 is-underlined" href="tel:2512334000">251-233-4000</a>
-                </div>
-                <div class="has-text-primary-dark is-size-3">Alabama Beach Chairs' Delivery Sets</div>
+                <?php require("./contact/_inc_contact_component.php"); ?>
+                <div class="has-text-primary-dark is-size-3 mt-4">Alabama Beach Chairs' Delivery Sets</div>
                 <div class="has-text-primary-dark is-size-4">
-                  2-person Chair Sets, with Umbrella
+                  (2-person Chair Sets with Umbrella)
                 </div>
+                <div class="has-text-danger-dark has-text-weight-semibold is-size-4">
+                  Minimum two sets for delivery
+                </div>
+                <div class="mb-2">Sorry we do not deliver to Fort Morgan</div>
                 <div class="columns is-mobile is-centered">
                   <section class="column has-text-left pb-0 is-size-5-mobile is-9-tablet is-7-desktop is-6-fullhd">
                     <ul class="is-size-4 abc-delivery-list-items">
                       <li>Two folding beach chairs with footrests</li>
                       <li>One 7' beach umbrella</li>
-                      <li>Umbrellas securely fastened by one of our friendly attendants</li>
-                      <li>Setup by 9 am, Pick up at 5 pm</li>
+                      <li>Umbrellas secured by one of our friendly attendants</li>
+                      <li>Setup by 9 am, Pick up after 5 pm</li>
                       <li>Setup/Pickup $30 per day</li>
+                      <li>No extra delivery fee for more sets</li>
                     </ul>
                     <div class="has-text-weight-bold is-size-4 mt-2 font-kalam has-text-danger-dark">
                       Setup/Pickup fees apply ONLY for Deliveries, NOT for
-                      <a class="is-underlined" href="#abc-properties">properties we serve</a> 
+                      <a class="is-underlined has-text-danger-dark" href="#abc-properties">
+                        properties we serve
+                      </a> 
                     </div>
                   </section>
                 </div>
@@ -131,9 +160,9 @@
                     <li>6 days: $150</li>
                     <li>7 days: $170</li>
                     <li class="mt-2 has-text-primary-dark has-text-weight-normal">
-                      ONE chair, OR umbrella
+                      Extra chair, OR umbrella
                     </li>
-                    <li>1 day: $20</li>
+                    <li>$15 each, per day</li>
                   </ul>
                 </section>
               </div>
@@ -143,7 +172,7 @@
         <!-- CONDO SERVICE -->
         <section class="mb-6 mx- abc-section" id="abc-condo-service">
           <header class="columns is-mobile is-centered">
-            <div class="column is-narrow is-size-4 has-background-danger-dark has-text-danger-light font-kalam">
+            <div class="column is-narrow is-size-3 has-background-danger-dark has-text-danger-light font-kalam">
             CONDO SERVICE
             </div>
           </header>
@@ -154,13 +183,12 @@
                 Alabama Beach Chairs provides folding beach chairs, beach umbrellas, kayaks, paddleboards, 
                 fun beach games, and a full rental inventory
               </header>
-              <div>
-                <span class="has-text-info-dark">Delivery fees</span> 
-                <span class="is-italic has-text-primary-dark">
-                  DO NOT APPLY&nbsp;<a class="is-underlined" href="#abc-properties">at properties we serve</a>  
-                </span>
+              <div class="has-text-danger-dark mb-5">Delivery fees DO NOT APPLY&nbsp;
+                <a class="is-underlined has-text-danger-dark href="#abc-properties">
+                  at properties we serve
+                </a>  
               </div>
-              <div class="is-flex is-justify-content-center my-1">
+              <div class="is-flex is-justify-content-center mb-2">
                 <div class="has-background-primary-dark has-text-warning is-size-3 font-pangolin px-2">
                   CONDO EQUIPMENT PRICING
                 </div>
@@ -207,7 +235,7 @@
         <section class="pt-2 mb-6 abc-section" id="abc-featured-property">
           <div class="pt-3">
             <header class="columns is-mobile is-centered mb-2">
-              <div class="column is-narrow is-size-4 has-background-danger-dark has-text-danger-light font-kalam">
+              <div class="column is-narrow is-size-3 has-background-danger-dark has-text-danger-light font-kalam">
               FEATURED PROPERTY
               </div>
             </header>
@@ -215,24 +243,24 @@
             <div class="is-block pt-1"><img src="./assets/images/properties/phoenix-vii-small.png"></img></div>
             <article class="columns is-mobile is-centered font-pangolin mt-1">
                <section class="column is-10 is-size-4 has-text-left">
-                  Alabama Beach Chairs proudly serves Phoenix VII, directly on the white sandy shores 
-                  of Orange Beach, Alabama, offering vacation rentals with great beach views 
+                Alabama Beach Chairs proudly serves Phoenix VII, directly on the white sandy shores 
+                of Orange Beach, Alabama, offering vacation rentals with great beach views 
                 from every unit.
               </section>
             </article>
           </div>
         </section>
         <!-- PROPERTIES -->
-        <section class="pt-2 mb-4 abc-section" id="abc-properties">
+        <section class="pt-2 mb-6 abc-section" id="abc-properties">
           <div class="pt-3">
           <header class="columns is-mobile is-centered mb-2">
-            <div class="column is-narrow is-size-4 has-background-danger-dark has-text-danger-light font-kalam">
+            <div class="column is-narrow is-size-3 has-background-danger-dark has-text-danger-light font-kalam">
             PROPERTIES WE SERVE
             </div>
           </header>
           <!-- PROPERTY IMAGE MODAL -->
           <div class="modal abc-property-img-modal">
-            <div class="modal-background"></div>
+            <div class="modal-background has-background-grey-dark"></div>
             <div class="modal-card">
               <header class="modal-card-head">
                 <!-- Title value gets passed in when property link is clicked -->
@@ -256,7 +284,7 @@
           <!-- END PROPERTY IMAGE MODAL -->
           <div class="is-size-5 font-pangolin">Tap or Click underlined links for image</div>
           <?php
-            require_once("./_inc_property_details.php");
+            require_once("_inc_property_details.php");
             foreach($abcPropertyDetailsByCity as $city => $propertyDetails) {
           ?>
             <article class="columns is-mobile is-centered font-pangolin">
@@ -272,9 +300,13 @@
                 </header>
                 <section class="abc-property-city-section is-size-4">
                   <ul>
-                  <?php foreach($propertyDetails as $details) { ?>
-                    <li data-abc-property-img-src="<?php echo $details["imgSrcPath"]; ?>">
-                      <a class="is-underlined"><?php echo $details["name"]; ?></a>
+                  <?php foreach($propertyDetails as $property) { ?>
+                    <li data-abc-property-img-src="<?php echo $property["imgSrcPath"]; ?>">
+                      <a class="is-underlined
+                                abc-text-orange
+                                has-text-weight-semibold">
+                        <?php echo $property["name"]; ?>
+                      </a>
                     </li>
                   <?php } ?>
                   </ul>
@@ -284,21 +316,18 @@
           <?php } ?>
         </section>
         <!-- ABOUT/CONTACT -->
-        <section class="pt-2 mb-6 abc-section abc-about-contact" id="abc-about-contact">
+        <section class="pt-2 abc-section abc-about-contact" id="abc-about-contact">
           <div class="pt-3">
             <header class="columns is-mobile is-centered mb-2">
-              <div class="column is-narrow is-size-4 has-background-danger-dark has-text-danger-light font-kalam">
+              <div class="column is-narrow is-size-3 has-background-danger-dark has-text-danger-light font-kalam">
                 ABOUT/CONTACT
               </div>
             </header>
           </div>
           <article class="columns is-mobile is-centered font-pangolin">
             <section class="column is-10">
-              <div class="has-text-link has-text-centered is-underlined is-size-2">
-                <a href="tel:2512334000">251-233-4000</a>
-              </div>
-              <div class="has-text-primary-dark has-text-centered is-size-4 mb-1">albeachchairs@gmail.com</div>
-              <div class="is-size-4 has-text-left">
+              <?php require("./contact/_inc_contact_component.php"); ?>
+              <div class="is-size-4 has-text-left mt-4">
                 <div class="mb-4">
                   Alabama Beach Chairs' proprietor has over three decades experience in the on-site, beach rental industry, which ensures the safest,
                   most professionally operated beach service for all guests, with customer service as priority one.                
@@ -316,6 +345,7 @@
         </section>
       </main>
     </div>
-    <!-- DOM MANIPULATION FUNCTIONS INCLUDED AT BOTTOM AFTER DOM HAS LOADED -->
-    <script src="./assets/js/abc-dom.js"></script>
+    <!-- LOAD SCRIPTS AFTER DOM HAS LOADED OR BEFORE MODAL FORM LOADS -->
+    <script src="./abc-index.js"></script>
+    <script src="./contact/abc-contact.js"></script>
   </body></html>
