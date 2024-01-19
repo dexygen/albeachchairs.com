1835e45204f6602b8c0bfe046778c0d5c9a9a381
George Jempty <jemptymethod@gmail.com>
2024-01-19 09:01:00 +0000

    Delivery page header

---
+++ abc24/delivery.content.php
@@ -0,0 +1,9 @@
+<?php require_once("./globals.def.php"); ?>
+
+<header>
+  <div class="has-text- is-size-3 font-kalam has-text-warning">Alabama Beach Chairs</div>
+  <img class="logo" src="./assets/images/abc-logo.png"></img>
+  <div class="has-text-danger-dark is-size-3 mb-1 has-text-weight-semibold font-kalam abc-line-height-1_1">
+	Delivers to Orange Beach and Gulf Shores!
+  </div>
+</header>
