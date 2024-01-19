c416a2b7e6f5b87124e45be629318a9c4a4970e6
George Jempty <jemptymethod@gmail.com>
2024-01-19 16:28:30 +0000

    Suspending revisions here, reverting to work on existing one-page site

--- abc24/delivery.content.php
+++ abc24/delivery.content.php
@@ -7,3 +7,42 @@
 	Delivers to Orange Beach and Gulf Shores!
   </div>
 </header>
+<main>
+	<div class="is-size-4 font-kalam has-text-primary-dark has-text-weight-semibold">Click or tap below to order</div>
+	<div class="mb-1">
+	  <a class="button 
+		is-rounded 
+		abc-order-delivery-button
+		abc-background-success-tint 
+		abc-text-orange
+		has-text-weight-semibold
+		is-underlined">
+		<div class="mx-2 abc-line-height-1_2 pb-2">DELIVERY<br />To my rental home</div>
+	  </a>
+	</div>
+    <div class="mb-2 has-text-weight-semibold">Sorry, we do not deliver to Fort Morgan</div>
+	<div class="is-flex is-justify-content-center">
+	  <div class="has-text-danger-dark is-size-3 font-kalam has-text-weight-semibold px-2 abc-line-height-1_1">
+		PRICING
+	  </div>
+	</div>
+	<div class="has-text-primary-dark is-size-5">
+	  ** Minimum two sets for delivery
+	</div>
+	<section class="is-size-4 has-text-weight-semibold mt-2">
+	  <div id="abc-chair-set-pricing" class="has-text-primary-dark abc-line-height-1_2">2-person Chair Sets, with Umbrella</div>
+	  <ul>
+		<li>1 day: $40</li>
+		<li>2 days: $70</li>
+		<li>3 days: $90</li>
+		<li>4 days: $110</li>
+		<li>5 days: $130</li>
+		<li>6 days: $150</li>
+		<li>7 days: $170</li>
+		<li class="mt-1 has-text-primary-dark">
+		  Extra chair, OR umbrella
+		</li>
+		<li>$15 each, per day</li>
+	  </ul>
+	</section>
+</main>
