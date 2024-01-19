e855c968266bf8869412a5636ab83f0a2e9a883e
George Jempty <jemptymethod@gmail.com>
2024-01-19 08:45:30 +0000

    Stubbed out link to delivery page

--- abc24/index.content.js
+++ abc24/index.content.js
@@ -1,6 +1,11 @@
+const abcDeliveryLink = document.querySelector(".abc-need-delivery-button");
 const abcCondosListModalEl = document.querySelector(".abc-condos-list-modal");
 const abcCondosListCloseEls = abcCondosListModalEl.querySelectorAll(".abc-condos-list-close");
 const abcCondosLinkEl = document.querySelector(".abc-condos-link");
+
+abcDeliveryLink.addEventListener("click", () => {
+	document.location.href = "./delivery.php";
+});
 
 abcCondosLinkEl.addEventListener("click", () => {
   abcCondosListModalEl.classList.add("is-active");
