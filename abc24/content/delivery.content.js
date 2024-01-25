f932ded2b47b020e737db7b7ab6c8864727e21ed
George Jempty <jemptymethod@gmail.com>
2024-01-25 22:56:17 +0000

    Enabled toggling between delivery info panels

--- abc24/content/delivery.content.js
+++ abc24/content/delivery.content.js
@@ -1,4 +1,25 @@
+const abcDeliveryTabs = document.querySelector(".abc-delivery-tabs");
 const abcReturnHomeLink = document.querySelector(".abc-return-home-link");
+
+[...abcDeliveryTabs.querySelectorAll("li")].forEach(tab => {
+	tab.addEventListener("click", () => {
+		let activeTab = abcDeliveryTabs.querySelector("li.is-active");
+		let inactiveTab = abcDeliveryTabs.querySelector("li:not(.is-active)");
+		
+		activeTab.classList.remove("is-active");
+		activeTab.querySelector("a").classList.add("has-background-light");
+		
+		inactiveTab.classList.add("is-active");
+		inactiveTab.querySelector("a").classList.remove("has-background-light");
+
+		let visibleInfoSection = document.querySelector(".abc-delivery-info-sections > section:not(.is-hidden)");
+		let hiddenInfoSection = document.querySelector(".abc-delivery-info-sections > section.is-hidden")
+		
+		visibleInfoSection.classList.add("is-hidden");
+		hiddenInfoSection.classList.remove("is-hidden");
+	});
+});
+
 abcReturnHomeLink.addEventListener("click", () => {
 	document.location.href = "./home.php";
 });
--- abc24/content/delivery.content.php
+++ abc24/content/delivery.content.php
@@ -37,60 +37,64 @@
 		</li>
 		<li>
 		  <a class="has-background-light">
+		    <!-- TODO: Remove dashed border from around span -->
 			<span>Delivery Pricing</span>
 		  </a>
 		</li>
 	  </ul>
 	</div>
 	<div class="abc-return-home-link is-underlined has-text-link mb-3">Or click or tap this link to return to home page</div>
-	<section class="abc-delivery-sets">
-		<div class="font-pangolin has-text-primary-dark is-size-4 has-text-weight-semibold abc-line-height-1_2">
-		  2-person Chair Sets with Umbrella
-		</div>
-		<!-- TODO: Eliminate as many layers of nesting and still acheive effect of "list-style: disc outside" -->
-		<article class="columns is-mobile is-centered font-pangolin">
-		  <div class="column is-10 has-text-centered">
-			<div class="columns is-mobile is-centered">
-			  <section class="column has-text-left pb-0 is-size-5-mobile is-9-tablet is-7-desktop is-6-fullhd">
-				<ul class="is-size-5 abc-delivery-list-items">
-				  <li>Two folding beach chairs with footrests</li>
-				  <li>One 7' beach umbrella</li>
-				  <li>Umbrellas secured by one of our friendly attendants</li>
-				  <li>Setup by 9 am, Pick up after 5 pm</li>
-				  <li>Setup/Pickup $30 per day</li>
-				  <li>No extra delivery fee for more sets</li>
-				</ul>
-			  </section>
+	<div class="abc-delivery-info-sections">
+	    <!-- TODO: Make appearance of two sections more consistent, e.g. fonts and colors -->
+		<section class="abc-delivery-sets">
+			<div class="font-pangolin has-text-primary-dark is-size-4 has-text-weight-semibold abc-line-height-1_2">
+			  2-person Chair Sets with Umbrella
 			</div>
-		  </div>
-		</article>
-	</section>
-    <section class="abc-delivery-pricing is-hidden">
-		<div class="is-flex is-justify-content-center">
-		  <div class="has-text-danger-dark is-size-3 font-kalam has-text-weight-semibold px-2 abc-line-height-1_1">
-			PRICING
-		  </div>
-		</div>
-		<div class="has-text-primary-dark is-size-5">
-		  ** Minimum two sets for delivery
-		</div>
-		<section class="is-size-4 has-text-weight-semibold mt-2">
-		  <div id="abc-chair-set-pricing" class="has-text-primary-dark abc-line-height-1_2">2-person Chair Sets, with Umbrella</div>
-		  <ul>
-			<li>1 day: $40</li>
-			<li>2 days: $70</li>
-			<li>3 days: $90</li>
-			<li>4 days: $110</li>
-			<li>5 days: $130</li>
-			<li>6 days: $150</li>
-			<li>7 days: $170</li>
-			<li class="mt-1 has-text-primary-dark">
-			  Extra chair, OR umbrella
-			</li>
-			<li>$15 each, per day</li>
-		  </ul>
+			<!-- TODO: Eliminate as many layers of nesting and still acheive effect of "list-style: disc outside" -->
+			<article class="columns is-mobile is-centered font-pangolin">
+			  <div class="column is-10 has-text-centered">
+				<div class="columns is-mobile is-centered">
+				  <section class="column has-text-left pb-0 is-size-5-mobile is-9-tablet is-7-desktop is-6-fullhd">
+					<ul class="is-size-5 abc-delivery-list-items">
+					  <li>Two folding beach chairs with footrests</li>
+					  <li>One 7' beach umbrella</li>
+					  <li>Umbrellas secured by one of our friendly attendants</li>
+					  <li>Setup by 9 am, Pick up after 5 pm</li>
+					  <li>Setup/Pickup $30 per day</li>
+					  <li>No extra delivery fee for more sets</li>
+					</ul>
+				  </section>
+				</div>
+			  </div>
+			</article>
 		</section>
-	</section>
+		<section class="abc-delivery-pricing is-hidden">
+			<div class="is-flex is-justify-content-center">
+			  <div class="has-text-danger-dark is-size-3 font-kalam has-text-weight-semibold px-2 abc-line-height-1_1">
+				PRICING
+			  </div>
+			</div>
+			<div class="has-text-primary-dark is-size-5">
+			  ** Minimum two sets for delivery
+			</div>
+			<section class="is-size-4 has-text-weight-semibold mt-2">
+			  <div id="abc-chair-set-pricing" class="has-text-primary-dark abc-line-height-1_2">2-person Chair Sets, with Umbrella</div>
+			  <ul>
+				<li>1 day: $40</li>
+				<li>2 days: $70</li>
+				<li>3 days: $90</li>
+				<li>4 days: $110</li>
+				<li>5 days: $130</li>
+				<li>6 days: $150</li>
+				<li>7 days: $170</li>
+				<li class="mt-1 has-text-primary-dark">
+				  Extra chair, OR umbrella
+				</li>
+				<li>$15 each, per day</li>
+			  </ul>
+			</section>
+		</section>
+	</div>
 </main>
 
 <script src="./content/delivery.content.js"></script>
