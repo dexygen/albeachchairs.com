fe68d58adb2e98dc98471feeed59ebefd3777f42
George Jempty <jemptymethod@gmail.com>
2024-04-23 18:33:36 +0100

    Increase font of condo pricing on larger resolutions

--- abc/includes/content/pricing/condos.pricing.php
+++ abc/includes/content/pricing/condos.pricing.php
@@ -1,5 +1,48 @@
+		  <div class="is-flex is-justify-content-center ">
+			<aside class="has-text-left is-size-5-mobile is-size-4-tablet mr-3">
+			  <div id="abc-chair-set-pricing" class="has-text-danger has-text-weight-semibold font-pangolin">
+				2-person Chair Sets, 
+				<br>with Umbrella
+			  </div>
+			  <ul class="has-text-weight-semibold">
+				<li>1 hour: $10</li>
+				<li>1 day: $40</li>
+				<li>2 days: $70</li>
+				<li>3 days: $100</li>
+				<li>4 days: $130</li>
+				<li>5 days: $160</li>
+				<li>6 days: $180</li>
+				<li>7 days: $200</li>
+				<li>After 2pm: $25 that day</li>
+				<li>After 3pm: $15 that day</li>
+				<li class="mt-2 has-text-danger has-text-weight-semibold font-pangolin">
+				  ONE chair, OR umbrella
+				</li>
+				<li>1 day: $25</li>
+			  </ul>
+			</aside>
+			<aside class="has-text-left is-size-5-mobile is-size-4-tablet ml-3">
+			  <div class="has-text-danger has-text-weight-semibold font-pangolin">
+			    Kayaks and Paddleboards, may switch between
+				<br>them during your time
+			  </div>
+			  <ul class="has-text-weight-semibold">
+				<li>1 hour: $30</li>
+				<li>2 hours: $50</li>
+				<li>1 day: $100</li>
+			  </ul>
+			  <div class="has-text-danger has-text-weight-semibold font-pangolin pt-2">Beach Games</div>
+			  <ul class="has-text-weight-semibold">
+				<li>1 hour: $15</li>
+				<li>2 hours: $20</li>
+				<li>1 day: $30</li>
+			  </ul>
+			</aside>
+		  </div>		  
+
+<!--		  
 		  <div class="columns is-mobile is-centered">
-			<aside class="column has-text-left is-size-5-mobile is-3-tablet">
+			<aside class="column has-text-left is-size-5-mobile is-size-4-tablet">
 			  <div id="abc-chair-set-pricing" class="has-text-danger has-text-weight-semibold font-pangolin">2-person Chair Sets, with Umbrella</div>
 			  <ul class="has-text-weight-semibold">
 				<li>1 hour: $10</li>
@@ -18,7 +61,7 @@
 				<li>1 day: $25</li>
 			  </ul>
 			</aside>
-			<aside class="column has-text-left is-size-5-mobile is-4-tablet">
+			<aside class="column has-text-left is-size-5-mobile is-size-4-tablet">
 			  <div class="has-text-danger has-text-weight-semibold font-pangolin">Kayaks and Paddleboards, may switch between the two during your time</div>
 			  <ul class="has-text-weight-semibold">
 				<li>1 hour: $30</li>
@@ -33,3 +76,4 @@
 			  </ul>
 			</aside>
 		  </div>
+-->
